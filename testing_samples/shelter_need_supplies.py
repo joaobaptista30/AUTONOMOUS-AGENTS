c862f1a5daa4e2390f5487aec74cce1c4643a974
João Baptista <joao.baptista@outlook.pt>
2024-11-18 18:03:04 +0000

    .

--- testing_samples/shelter_need_supplies.py
+++ testing_samples/shelter_need_supplies.py
@@ -12,15 +12,15 @@ async def main():
 
     # iniciar agents manualmente para teste
     shelter1 = ShelterAgent("shelter1@localhost", "password", environment.blocks["AH"], environment)
-    supplyer1 = SupplierAgent("supplyer1@localhost", "password", environment.blocks["BD"], environment)
+    supplier1 = SupplierAgent("supplier1@localhost", "password", environment.blocks["BD"], environment)
 
     environment.agents_contact["shelter"] = [shelter1]
-    environment.agents_contact["supplyer"] = [supplyer1]
+    environment.agents_contact["supplier"] = [supplier1]
 
     # Start all agents
 
     await shelter1.start(auto_register=True)
-    await supplyer1.start(auto_register=True)
+    await supplier1.start(auto_register=True)
     print()
 
     shelter1.current_supplies = 100
@@ -30,11 +30,11 @@ async def main():
 
     print("shelter na pos: ", shelter1.position.name)
     print("mantimentos no shelter: ", shelter1.current_supplies)
-    print("posicao atual do supplie apos entregar mantimentos ao shelter: ", supplyer1.position.name)
+    print("posicao atual do supplie apos entregar mantimentos ao shelter: ", supplier1.position.name)
 
     # Stop agents after the test
     await shelter1.stop()
-    await supplyer1.stop()
+    await supplier1.stop()
 
 
 if __name__ == "__main__":
