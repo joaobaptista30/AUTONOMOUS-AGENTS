d60062b36852f65f0ef7212be10683dbe1969272
João Baptista <joao.baptista@outlook.pt>
2024-11-19 02:00:23 +0000

    quase terminado

---
+++ codigo_final/disasters.py
@@ -0,0 +1,90 @@
+import asyncio
+import random
+from spade import agent
+from spade.behaviour import PeriodicBehaviour
+
+from environment import load_env
+
+
+class Disaster(agent.Agent):
+    # vai invocar um desastre para causar danos e assim começarem os salvamentos
+    def __init__(self, jid, password, env):
+        super().__init__(jid, password)
+        self.env = env
+        self.distaster_type = ["flood", "hurricane", "earthquake"]
+        self.damage_spread = {"flood": [[10,10],[12,8],[14,6],[16,4],[18,2]],
+                              "hurricane": [[10,15],[15,11],[20,7],[25,3]],
+                              "earthquake": [[7,20],[10,15],[13,10],[16,5]]}
+        '''
+        flood: cria um dano de 10 num raio de 10m, depois a cada 2m de distancia vai diminuir 2 em dano (dist-dano) 10-10 | 12-8 | 14-6 | 16-4 | 18-2 | X-0
+        hurricane: cria um dano de 15 num raio de 10m, depois a cada 5m de distancia vai diminuir 4 em dano (dist-dano) 10-15 | 15-11 | 20-7 | 25-3 | X-0 
+        earthquake: cria um dano de 20 num raio de 7m, depois a cada 3m de distancia vai diminuir 5 em dano (dist-dano) 7-20 | 10-15 | 13-10 | 16-5 | X-0
+        '''
+
+    class StartDisaster(PeriodicBehaviour):
+        async def on_start(self):
+            await asyncio.sleep(10)
+
+        async def run(self):
+            afetados = []
+            disaster_selected = random.choice(self.agent.distaster_type)
+            disaster_epicenter = random.choice(list(self.agent.env.blocks.values()))
+            environment = self.agent.env
+            max_dist = self.agent.damage_spread[disaster_selected][-1][-2]
+
+            # adicionar o dano em casa bloco
+            visited = []
+            queu = [[disaster_epicenter,0]]
+
+            while queu:
+                curr, dist = queu.pop(0)
+                visited.append(curr.name)
+
+                if curr.block_type != "shelter":
+                    for dist_center, damage_taken in self.agent.damage_spread[disaster_selected]:
+                        if dist <= dist_center:
+                            curr.damage = damage_taken
+                            afetados.append(curr.name)
+                            break
+
+                for adj in curr.adj:
+                    if adj.destiny.name not in visited and dist+adj.distance <= max_dist:
+                        queu.append([adj.destiny,dist+adj.distance])
+            afetados.pop(0) # primeiro vai ser a disaster_epicenter e nao precisamos
+            print(f"Aviso uma catastrofe ({disaster_selected}) iniciou se em {disaster_epicenter.name} e espalhou-se pelos blocos {afetados}")
+
+    async def setup(self):
+        self.add_behaviour(self.StartDisaster(period=120)) # vai iniciar um desastre a cada 60 segundos
+
+
+class RepairMan(agent.Agent):
+    # vai repara as casa com danos para permitir os civil regressarem
+    def __init__(self, jid, password, env):
+        super().__init__(jid, password)
+        self.env = env
+
+    class FixDamage(PeriodicBehaviour):
+        async def run(self):
+            for block in list(self.agent.env.blocks.values()):
+                block.damage = max(0, block.damage-5)
+
+    async def setup(self):
+        self.add_behaviour(self.FixDamage(period=40)) # vai reparar os danos em 5 a cada 60 segundos
+
+
+async def main():
+    env = load_env("city_desing.txt")
+    disasters = Disaster("disastermanager@localhost","password",env)
+    await disasters.start(auto_register=True)
+
+    repairman = RepairMan("repairman@localhost","password",env)
+    await repairman.start(auto_register=True)
+
+    await asyncio.sleep(15)
+
+    await disasters.stop()
+    await repairman.stop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ codigo_final/environment.py
@@ -0,0 +1,101 @@
+class Connections:  # edge
+    def __init__(self, destiny, distance, blocked=False):
+        self.destiny = destiny  # referencia para o block
+        self.distance: int = distance
+        self.blocked: bool = blocked
+
+    def get_destiny(self):
+        return self.destiny
+
+    def get_distance(self) -> int:
+        return self.distance
+
+    def set_distance(self, new_distance: int):
+        self.distance = new_distance
+
+
+class Block:  # nodes
+    def __init__(self, name, block_type, zone, adj_zone):
+        self.name: str = name
+        self.block_type = block_type
+        self.zone: int = zone
+        self.adj_zone: [int] = adj_zone
+        self.adj = []  # referencia para connections
+        self.disaster: str = "clear"
+        self.damage: int = 0
+
+    def get_adj(self) -> list:
+        return self.adj
+
+    def set_disaster_damage(self, disaster, damage):
+        self.disaster = disaster
+        self.damage = damage
+
+
+class Environment:
+    def __init__(self):
+        self.blocks = {}
+        self.agents_contact = {}  # like yellow pages for communication ['rescuer','supplier','shelter'] storing a pointer to the agent
+        # performance stats
+        self.civilians_rescued = 0
+        self.supplies_delivered = 0
+        self.total_rescuers_trips = 0
+        self.total_rescuers_time_traveled = 0
+        self.total_suppliers_trips = 0
+        self.total_suppliers_time_traveled = 0
+
+
+    def display(self) -> None:  # currently show the input to analise if it is correct
+        print(self.blocks)
+        for node in self.blocks.values():
+            print(node.name, node.zone, node.adj_zone)
+            for adj in node.adj:
+                print(adj.destiny, adj.distance)
+
+
+def load_env(env_desing_path) -> Environment:
+    """
+    file format:
+    1st line -> number os blocks (nodes)
+    n_blocks lines -> <name>,<type>,<zone>,<neighbour_zones> // type:{house,condo,shelter,supplier,empty}
+    n_blocks lines -> same order input as the nodes creation to reference the adj nodes
+    example:
+        4
+        A,house,1,2 3
+        B,condo,2,1
+        C,empty,3,1
+        D,shelter,1,
+        B 4,C 5,D 5  -- connections from A to _ with cost x
+        A 10         -- connections from B to _ with cost x
+                     -- connections from C to _ with cost x
+        A 3          -- connections from D to _ with cost x
+    /example
+        in this example C does not have an exit, something that in practice will not happen
+        but is allowed for other project
+    """
+    envir = Environment()
+    with open(env_desing_path, 'r') as file:
+        lines = file.read().splitlines()
+        i = 1
+        while i <= int(lines[0]):  # create blocks (nodes)
+            name, block_type, zone, adj_z = lines[i].split(",")
+            adj_zones = [int(i) for i in adj_z.split(" ") if len(adj_z) > 0]
+            envir.blocks[name] = (Block(name, block_type, int(zone), adj_zones))
+            i += 1
+
+        for node in envir.blocks.values():
+            if len(lines[i]) == 0:
+                i += 1
+                continue
+            conn = [k for k in lines[i].split(",")]
+            for j in range(len(conn)):
+                adj_name, dist = conn[j].split(" ")
+                node.adj.append(Connections(envir.blocks[adj_name], int(dist)))
+            i += 1
+
+    return envir
+
+
+if __name__ == "__main__":
+    env = load_env("./city_desing.txt")
+    env.display()
